1118454bf40599ae7af43a21e6e7b2562ccdd1b2
0xj4an <personal@0xj4an.xyz>
2026-05-21 15:31:49 -0500

    chore: add talentapp domain verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,10 @@ export const metadata: Metadata = {
     title: "MiniKlaim",
     description: "Run it. Klaim it.",
   },
+  other: {
+    "talentapp:project_verification":
+      "1ab9d6be6ebab071c11210474ceed66044004a2994d648f70d8e5b8d37836ef6da1de20f5b85048496de08da8297d276d31ad97334f88b05f2a919c5e9898ab8",
+  },
 };
 
 export const viewport: Viewport = {
